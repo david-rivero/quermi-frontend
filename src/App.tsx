0bbe265a7b59f7757321553b51fe093fbe64a59f
David Rivero <david.davidsociety.rivero@gmail.com>
2024-09-18 23:21:48 +0100

    Create Chat module

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { Login, Register, Calendar } from './views';
+import {
+  Login,
+  Register,
+  Calendar,
+  ChatInbox,
+  ChatSession,
+} from './views';
 
 import './App.css';
 
@@ -12,6 +18,8 @@ const App: React.FC = () => {
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register/:stepId" element={<Register />} />
         <Route path="/calendar" element={<Calendar />} />
+        <Route path="/chat/inbox" element={<ChatInbox />} />
+        <Route path="/chat/session/:chatId" element={<ChatSession />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/views/chat/session/Session.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ChatSession: React.FC = () => {
+  return (
+    <></>
+  );
+};
