8a6175d975fa73fca8f9f808da844cec4aa392d0
David Rivero <david.davidsociety.rivero@gmail.com>
2024-09-18 23:17:53 +0100

    Create Auth module

---
+++ src/views/auth/register/Register.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Step1, Step2, Step3, Step4 } from './components';
+
+export const Register: React.FC = () => {
+  return (
+    <></>
+  );
+};
---
+++ src/views/auth/register/components/Step1.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Step1: React.FC = () => {
+  return (
+    <></>
+  );
+};
